949019febe40e6021e813d9f2f77a194eafcc740
aditi <anand.av@hotmail.com>
2025-03-02 20:23:17 +0530

    adding dome code for login

--- Program.cs
+++ Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
+        SupabaseDemo supabaseDemo = new SupabaseDemo();
+        supabaseDemo.StartSignIn();
         Console.WriteLine("Hello, World!");
     }
+
+    //    public static MauiApp CreateMauiApp()
+    //    {
+    //        // ...
+    //        var builder = MauiApp.CreateBuilder();
+    //
+    //        var url = Environment.GetEnvironmentVariable("SUPABASE_URL");
+    //        var key = Environment.GetEnvironmentVariable("SUPABASE_KEY");
+    //        var options = new SupabaseOptions
+    //        {
+    //            AutoRefreshToken = true,
+    //                             AutoConnectRealtime = true,
+    //                             // SessionHandler = new SupabaseSessionHandler() <-- This must be implemented by the developer
+    //        };
+    //
+    //        // Note the creation as a singleton.
+    //        builder.Services.AddSingleton(provider => new Supabase.Client(url, key, options));
+    //    }
 }
--- SupabaseDemo.cs
+++ SupabaseDemo.cs
@@ -1,8 +1,26 @@
-﻿namespace superbase.demo;
-using supabase;
-
-
-public class SupabaseDemo
+﻿using Supabase.Gotrue;
+using Supabase.Gotrue.Interfaces;
+using System.Threading.Tasks;
+using static Supabase.Gotrue.Constants;
+namespace superbase.demo
 {
+    public class SupabaseDemo
+    {
+        public SupabaseDemo()
+        {
+        }
 
+        public  async Task  StartSignIn()
+        {
+            Console.WriteLine("Start Sign In called.");
+            Client client = new Client(new ClientOptions { AllowUnconfirmedUserSessions = true });
+            var status = new NetworkStatus();
+            status.Client = client;
+            await status.StartAsync("");
+            var signInUrl = client.SignIn(Provider.Github);
+            Console.WriteLine(signInUrl);
+
+        }
+
+    }
 }
